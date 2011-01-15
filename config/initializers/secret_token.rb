855d8d1750c566caa452fb31cde205092a0990d7
eduardo <eduardo.desouza@gmail.com>
2011-01-14 23:40:07 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EduardoApp::Application.config.secret_token = '14acec4bb174a1f8b690993935f192e2c5288f112754c5867e5dc9aa58995f3ad9679dfd474452976e6a56e0f3c5dcf461d4bdffb53484d8fee0116033bff4ff'
